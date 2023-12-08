7819c56f27c243a0aab4e865b90fa2ae0fe502c4
dorian921 <144111488+dorian921@users.noreply.github.com>
2023-12-08 09:57:38 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function openNav() {
+    var sidebar = document.getElementById("mysidebar")
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+}
+
